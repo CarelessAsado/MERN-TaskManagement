0674918221cd5cfd417de090956c15175cdebbcc
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-02-21 15:01:43 -0300

    process.env.NODE_ENV

--- front/src/API/url.js
+++ front/src/API/url.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const url = "https://react-task-management.herokuapp.com/api/";
+const url =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://react-task-management.herokuapp.com/api/";
 
 export default axios.create({
   baseURL: url,
