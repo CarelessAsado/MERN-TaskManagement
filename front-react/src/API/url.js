5366be0e4f300b6b47db5e89ebc031370f62741d
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-02-20 22:06:58 -0300

    hola

--- front-react/src/API/url.js
+++ front-react/src/API/url.js
@@ -1,11 +1,9 @@
 import axios from "axios";
+
 const url = process.env.NODE
-  ? "https://react-todolist-authentication.herokuapp.com/api/users/auth"
-  : "http://localhost:5000/api/users/auth";
+  ? "https://react-todolist-authentication.herokuapp.com/api/"
+  : "http://localhost:5000/api/";
+
 export default axios.create({
   baseURL: url,
 });
-
-export const urlTarea =
-  "https://react-todolist-authentication.herokuapp.com/api/tareas";
-export const urlUsers = url;
