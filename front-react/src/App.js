46574cc39914bb9b076abf6db2c96838ca88c5c0
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-02-20 21:57:12 -0300

    changes

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -2,24 +2,16 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 /*-----------PAGES**********************/
-import { Header } from "./components/Header";
-import { TodoForm } from "./components/TodoForm";
+
 import { Register } from "./pages/Register";
+import { Main } from "./pages/Main";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header></Header>
-                <TodoForm></TodoForm>
-              </>
-            }
-          ></Route>
+          <Route path="/" element={<Main />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Register />}></Route>
         </Routes>
---
+++ front-react/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { TodoForm } from "../components/TodoForm";
+export const Main = () => {
+  return (
+    <>
+      <Header />
+      <TodoForm />
+    </>
+  );
+};
