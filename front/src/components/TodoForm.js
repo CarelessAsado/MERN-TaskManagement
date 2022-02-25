5eb132b7cad96dc17b2aace4de795c0e036700d7
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-02-25 00:18:40 -0300

    deploy netl

--- front/src/components/TodoForm.js
+++ front/src/components/TodoForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+/* import { useNavigate } from "react-router-dom"; */
 import { TodoItem } from "./TodoItem";
 import { tareasAPI } from "../API/tareasAPI";
 import { useGlobalContext } from "../Hooks/useGlobalContext";
