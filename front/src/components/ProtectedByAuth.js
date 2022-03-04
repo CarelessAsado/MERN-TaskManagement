18cd3b1d56122079b19875e0c946eff03d36cf1c
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-03-04 12:42:37 -0300

    n

--- front/src/components/ProtectedByAuth.js
+++ front/src/components/ProtectedByAuth.js
@@ -5,7 +5,7 @@ export const ProtectedByAuth = () => {
   const { user } = useGlobalContext();
   const location = useLocation();
   console.log(
-    "PROTECTED BY AUTH, after persist component, siempre hay user en esta etapa"
+    "PROTECTED BY AUTH, after persist component, si hay user sigue camino, si no hay user Navigate to Login"
   );
   return user ? (
     <Outlet></Outlet>
